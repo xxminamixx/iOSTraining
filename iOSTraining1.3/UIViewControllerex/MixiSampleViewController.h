8832503ae4ef66f51cd6052e996d1ee140812563
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-09 15:58:17 +0900

    chapter1.4追加

---
+++ iOSTraining1.3/UIViewControllerex/MixiSampleViewController.h
@@ -0,0 +1,16 @@
+//
+//  MixiSampleViewController.h
+//  UIViewControllerex
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiSampleViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UILabel *label;
+
+
+@end
---
+++ iOSTraining1.3/UIViewControllerex/MixiSampleViewController.m
@@ -0,0 +1,13 @@
+//
+//  MixiSampleViewController.m
+//  UIViewControllerex
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "MixiSampleViewController.h"
+
+@implementation MixiSampleViewController
+
+@end
