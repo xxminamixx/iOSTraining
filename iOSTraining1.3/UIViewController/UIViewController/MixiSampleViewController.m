e99f54003c8c885647469238b6d4b33a0b2bf809
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-09 11:52:15 +0900

    xibファイル追加 初期化完了

---
+++ iOSTraining1.3/UIViewController/UIViewController/MixiSampleViewController.m
@@ -0,0 +1,38 @@
+//
+//  MixiSampleViewController.m
+//  UIViewController
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "MixiSampleViewController.h"
+
+@interface MixiSampleViewController ()
+
+@end
+
+@implementation MixiSampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    MixiSampleViewController *sampleVC = [[MixiSampleViewController alloc] initWithNibName:@"MixiSampleViewController" bundle:nil];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
