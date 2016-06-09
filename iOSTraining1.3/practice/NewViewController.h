9b5fedda2d0ea1059b14c534a61b35523444f125
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-09 16:29:11 +0900

    演習問題追加

---
+++ iOSTraining1.3/practice/NewViewController.h
@@ -0,0 +1,16 @@
+//
+//  NewViewController.h
+//  practice
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)buttonAction:(id)sender;
+@end
