c3b9ebd8bed73d6aac5d79ca89ba5842d52bacae
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-09 10:28:03 +0900

    カテゴリファイル追加

---
+++ iOSTraining1.1/mixiSample/mixiSample/NSString+HogeAddition.h
@@ -0,0 +1,13 @@
+//
+//  NSString+HogeAddition.h
+//  mixiSample
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HogeAddition)
+
+@end
---
+++ iOSTraining1.1/mixiSample/mixiSample/NSString+HogeAddition.m
@@ -0,0 +1,13 @@
+//
+//  NSString+HogeAddition.m
+//  mixiSample
+//
+//  Created by Minami Kyohei on 2016/06/09.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "NSString+HogeAddition.h"
+
+@implementation NSString (HogeAddition)
+
+@end
