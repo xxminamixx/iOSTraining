980a56717d925fa13756f57d1f8943273a8ddf8b
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-10 18:01:12 +0900

    chapter5.1追加 カメラへのアクセス

---
+++ iOSTraining5.1/cameraPicker/cameraPicker/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  cameraPicker
+//
+//  Created by Minami Kyohei on 2016/06/10.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cameraroll:(id)sender {
+    UIImagePickerController *imagePickerVC = [[UIImagePickerController alloc] init];
+    // UIImagePickerControllerSourceTypeSavedPhotosAlbum だと直接写真選択画面
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    // 選択したメディアの編集を可能にするかどうか
+    imagePickerVC.allowsEditing = YES;
+    
+    // 選択可能なメディアの制限 デフォルトは photo のみ。
+    // movie を選択可能にするには
+    imagePickerVC.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:imagePickerVC.sourceType];
+    imagePickerVC.delegate = self;
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+
+@end
