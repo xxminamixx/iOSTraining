d81cd428cc723b0af6716ce08c975c37820a24ed
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-10 16:06:19 +0900

    chapter4.2追加

---
+++ iOSTraining4.2/TableViewSample/TableViewSample/TVSViewController.h
@@ -0,0 +1,13 @@
+//
+//  TVSViewController.h
+//  TableViewSample
+//
+//  Created by Minami Kyohei on 2016/06/10.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVSViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
