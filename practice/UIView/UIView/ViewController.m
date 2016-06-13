6726baf5b2276f704501483dc00b66d3470c20cc
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-13 16:40:28 +0900

    エラーが発生

--- practice/UIView/UIView/ViewController.m
+++ practice/UIView/UIView/ViewController.m
@@ -7,13 +7,16 @@
 //
 
 #import "ViewController.h"
-#import "tableView.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *label;
+- (IBAction)input:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (weak, nonatomic) IBOutlet UITableViewCell *cell1;
 
 @end
 
-@implementation ViewController
+@implementation ViewController : UIViewController <tableView>
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,22 +25,30 @@
     
     //xibをstoryboardに追加する
     //調査した中で一番簡潔であった
-    
+    /*
     UINib *nib = [UINib nibWithNibName:@"tableView" bundle:nil];
     [nib instantiateWithOwner:self options:nil];
-    
+    */
 }
+
+// 設定（セル）
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
+                                                            forIndexPath:indexPath];
+    NSMutableArray *_objects;
+    NSDate *object = _objects[indexPath.row];
+    cell.textLabel.text = [object description];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-- (void)copyText:(NSString*) label
-{
-    NSLog(@"%@", label);
-}
-*/
  
+- (IBAction)input:(id)sender {
+    NSLog(@"inner");
+}
 @end
--- practice/UIView/UIView/tableView.m
+++ practice/UIView/UIView/tableView.m
@@ -6,8 +6,6 @@
 //  Copyright © 2016年 Minami Kyohei. All rights reserved.
 //
 
-#import "tableView.h"
-
 // Delegate用のプロトコルを宣言 @property id <ここに対応> delegate;
 @protocol TableView <NSObject>
 
@@ -15,11 +13,8 @@
 @end
 
 @interface tableView ()
-//- (IBAction)addText:(id)sender;
-//- (IBAction)removeText:(id)sender;
+- (IBAction)pushAction1:(id)sender;
 @property (weak, nonatomic) id <TableView> delegate;
-@property (weak, nonatomic) IBOutlet UITextField *text;
-
 @end
 
 @implementation tableView
@@ -34,7 +29,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -42,16 +37,17 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
+
+
 /*
-- (IBAction)addText:(id)sender {
-    //[self.delegate copyText:_text.text];
-}
-
-
 - (IBAction)removeText:(id)sender {
     
 }
- */
+*/
 
+- (IBAction)pushAction1:(id)sender {
+    NSString *str = @"delegate clear";
+    [self.delegate copyText: str];
+}
 @end
