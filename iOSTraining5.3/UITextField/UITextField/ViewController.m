2172ad8a60a5f479ee92bc22e0adc4f194ac8c7f
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-13 11:27:04 +0900

    UITextFiledを扱う

--- iOSTraining5.3/UITextField/UITextField/ViewController.m
+++ iOSTraining5.3/UITextField/UITextField/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *textFieldCount;
+@property (weak, nonatomic) IBOutlet UILabel *textViewCount;
 
 @end
 
@@ -16,7 +20,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // textFieldはUITextField型の変数
+    _textField.delegate = self;
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +33,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (BOOL)textFieldShouldReturn:(UITextField *)targetTextField {
+    
+    
+    if (_textField.text.length) {
+        NSInteger count = _textView.text.length;
+        NSString *str = [[NSString stringWithFormat:@"%ld", (long)count];
+        
+    }
+
+    
+    if (_textView.text.length) {
+        _textViewCount.text = _textViewCount.text.length;
+    }
+    
+    
+    
+        
+    // textFieldを最初にイベントを受け取る対象から外すことで、
+    // キーボードを閉じる。
+    [targetTextField resignFirstResponder];
+    
+    return YES;
+}
 @end
