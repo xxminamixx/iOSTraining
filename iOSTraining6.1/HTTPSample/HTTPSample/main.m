0dd9215db032c429b66905825df035e747401431
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-14 13:32:57 +0900

    chapter6.1

---
+++ iOSTraining6.1/HTTPSample/HTTPSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HTTPSample
+//
+//  Created by Minami Kyohei on 2016/06/14.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HSAppDelegate class]));
+    }
+}
