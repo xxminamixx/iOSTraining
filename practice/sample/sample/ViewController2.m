6156ec0eb406c239c48178f3b0d2e3301369208f
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-14 16:19:03 +0900

    Navigation Controllerの練習

---
+++ practice/sample/sample/ViewController2.m
@@ -0,0 +1,44 @@
+//
+//  ViewController2.m
+//  sample
+//
+//  Created by Minami Kyohei on 2016/06/14.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+@property (weak, nonatomic) IBOutlet UILabel *lebel1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+- (IBAction)reson:(id)sender;
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)reson:(id)sender {
+    _lebel1.text = @"フライパン\n鉄でできていますが\n名前にパンが付いています";
+}
+@end
