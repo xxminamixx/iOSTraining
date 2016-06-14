bf52da65565c6c0625b2a6cdca683961c91b4d70
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-14 15:07:29 +0900

    chapter8.2追加

---
+++ iOSTraining8.2/GCD/GCD/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  GCD
+//
+//  Created by Minami Kyohei on 2016/06/14.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    for (NSInteger i = 0 ; i < 100; ++i) {
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_sync(queue, ^{
+            NSLog(@"%ld", (long)i);
+        });
+    }
+    
+    NSLog(@"here!!");
+    
+    
+    dispatch_group_t group = dispatch_group_create(); // 1. dispatch group の生成
+    
+    for (NSInteger i = 0 ; i < 100; ++i) {
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        
+        // 2. タスクをdispatch_queue に追加
+        dispatch_group_async(group, queue, ^{
+            NSLog(@"%ld", (long)i);
+        });
+        
+    }
+    
+    // 3. タスクの終了まで待機
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    
+    NSLog(@"here!!");
+    
+    // 4. 不要になったgroupをrelease
+    //dispatch_release(group);　//前は自動で行われなかった？
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOSTraining8.2/GCD/GCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD
+//
+//  Created by Minami Kyohei on 2016/06/14.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
