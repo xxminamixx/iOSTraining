454829eec2a040f623bf4fb226888ef866ac93fd
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-14 13:48:16 +0900

    NSFileManagerを扱う

--- iOSTraining7.2/NSFileManager/NSFileManager/ViewController.m
+++ iOSTraining7.2/NSFileManager/NSFileManager/ViewController.m
@@ -16,7 +16,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSArray *urls = [fileManager URLsForDirectory:NSDocumentDirectory
+                                        inDomains:NSUserDomainMask];
+    NSLog(@"%@", urls[0]);
+    
+    NSString *documentURLString = [(NSURL*)urls[0] absoluteString];
+    NSURL *fileURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@save.xml", documentURLString]];
+    NSLog(@"%@", fileURL);
+    
+    NSDictionary *savedDict = @{@"key1":@"value1", @"key2":@"value2"};
+    [savedDict writeToURL:fileURL atomically:YES];
+    
+    if([fileManager fileExistsAtPath:fileURL.path]){ //[1] ファイルパスが存在するかどうかを確認
+        NSDictionary *readDict = [NSDictionary dictionaryWithContentsOfURL:fileURL]; //[2] 保存したファイルを dictionary として生成
+        NSLog(@"%@", readDict);
+    }else{
+        NSLog(@"not exist");
+    }
+    
+    if([fileManager fileExistsAtPath:fileURL.path]){
+        NSError *error = nil;
+        [fileManager removeItemAtURL:fileURL error:&error];
+    }else{
+        NSLog(@"not exist");
+    }
 }
 
 - (void)didReceiveMemoryWarning {
