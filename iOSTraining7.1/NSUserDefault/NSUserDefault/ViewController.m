0be8e571ac0f36b85471e5282242643e9f02bc37
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-14 13:28:24 +0900

    配列を出力

--- iOSTraining7.1/NSUserDefault/NSUserDefault/ViewController.m
+++ iOSTraining7.1/NSUserDefault/NSUserDefault/ViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableArray *array = [NSMutableArray new];
+    NSArray *leng = @[@"日本", @"アメリカ", @"フランス"];
+    array = [leng mutableCopy];
+    
+    //配列情報を保持
+    [[NSUserDefaults standardUserDefaults] setObject:array forKey:@"array"];
+    
+    NSLog(@"%@", [[NSUserDefaults standardUserDefaults] objectForKey:@"array"]);
+    
 }
 
 - (void)didReceiveMemoryWarning {
