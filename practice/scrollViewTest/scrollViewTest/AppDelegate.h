72d3b79b6040d90eb47c2485184cc863d7574f38
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-15 16:58:04 +0900

    tableViewのスクロールできず

---
+++ practice/scrollViewTest/scrollViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollViewTest
+//
+//  Created by Minami Kyohei on 2016/06/15.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practice/scrollViewTest/scrollViewTest/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  scrollViewTest
+//
+//  Created by Minami Kyohei on 2016/06/15.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    // Do any additional setup after loading the view, typically from a nib.
+    _scrollView.contentSize =　_tableView.frame.size;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//必須メソッド
+//tableViewのcellの数を返す
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 100;
+}
+
+//tableViewに表示するcellを返す
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [UITableViewCell new];
+    cell.textLabel.text = @"テスト";
+    return cell;
+}
+
+//tableViewのcellの高さを返す
+-(CGFloat) tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath {
+    
+    return 100;
+}
+
+
+@end
